dae26286b170ba40010545c4e25dc137de71143c
meskill <8974488+meskill@users.noreply.github.com>
2022-04-24 12:26:20 +0000

    build: simplify build script

--- src/lib.rs
+++ src/lib.rs
@@ -83,27 +83,24 @@
 //!
 //! ```
 //! use std::env;
-//! use std::path::{Path, PathBuf};
-//!
-//! fn get_output_path() -> PathBuf {
-//!     //<root or manifest path>/target/<profile>/
-//!     let manifest_dir_string = env::var("CARGO_MANIFEST_DIR").unwrap();
-//!     let build_type = env::var("PROFILE").unwrap();
-//!     let target = env::var("TARGET").unwrap();
-//!
-//!     Path::new(&manifest_dir_string)
-//!         .join("target")
-//!         .join(target)
-//!         .join(build_type)
-//! }
+//! use std::path::Path;
 //!
 //! fn main() -> std::io::Result<()> {
 //!     println!("cargo:rerun-if-changed=sdk");
 //!
 //!     let current_dir = env::current_dir()?;
-//!     let out_dir = get_output_path();
+//!     let out_dir = env::var("OUT_DIR").unwrap();
+//!
 //!     let from_path = current_dir.join("sdk");
-//!     let dest_path = Path::new(&out_dir).join("sdk");
+//!
+//!     let dest_path = Path::new(&out_dir)
+//!         .parent()
+//!         .unwrap()
+//!         .parent()
+//!         .unwrap()
+//!         .parent()
+//!         .unwrap()
+//!         .join("sdk");
 //!
 //!     if !dest_path.exists() {
 //!         copy_dir::copy_dir(from_path, dest_path)?;
