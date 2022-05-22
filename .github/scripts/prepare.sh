57c16150f8946980f757b745e6b43c6889b800a9
meskill <8974488+meskill@users.noreply.github.com>
2022-05-22 17:28:32 +0000

    ci: fix publish

--- .github/scripts/prepare.sh
+++ .github/scripts/prepare.sh
@@ -4,4 +4,4 @@ cargo install cargo-edit cargo-readme
 
 cargo set-version $1
 cargo readme > README.md
-cargo check # update Cargo.lock
+cargo check --target=x86_64-pc-windows-gnu # update Cargo.lock
