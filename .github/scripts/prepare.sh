649624c4b436085c5d0636905590a499a636d3b3
meskill <8974488+meskill@users.noreply.github.com>
2022-05-23 10:08:11 +0000

    docs: fix README.md
    also disable readme generation for some time

--- .github/scripts/prepare.sh
+++ .github/scripts/prepare.sh
@@ -1,7 +1,8 @@
 set -e
 
-cargo install cargo-edit cargo-readme
+cargo install cargo-edit # cargo-doc2readme
 
 cargo set-version $1
-cargo readme > README.md
+# use [doc2readme](https://github.com/msrd0/cargo-doc2readme) when new version will be released
+# cargo doc2readme --expand-macros --out README.md
 cargo check --target=x86_64-pc-windows-gnu # update Cargo.lock
