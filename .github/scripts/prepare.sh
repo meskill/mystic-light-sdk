0ae2f32c03f114896f87df5f175d6c681747004a
meskill <8974488+meskill@users.noreply.github.com>
2022-05-26 13:35:52 +0000

    docs: autogenerate README.md

--- .github/scripts/prepare.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-cargo install cargo-edit # cargo-doc2readme
-
-cargo set-version $1
-# use [doc2readme](https://github.com/msrd0/cargo-doc2readme) when new version will be released
-# cargo doc2readme --expand-macros --out README.md
-cargo check --target=x86_64-pc-windows-gnu # update Cargo.lock
